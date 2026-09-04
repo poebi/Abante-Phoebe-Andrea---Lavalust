30b6a6a7af1eb61f5b41a1efad5b0a5df0b02ef3
poebi <phoebeabante18@gmail.com>
2026-09-04 09:53:59 +0800

    Update fallback credentials to Aiven

--- app/config/database.php
+++ app/config/database.php
@@ -2,12 +2,12 @@
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
-    'driver'   => getenv('DB_DRIVER') ?: 'mysql',
-    'hostname' => getenv('DB_HOST') ?: 'localhost',
-    'port'     => getenv('DB_PORT') ?: '3306',
-    'username' => getenv('DB_USERNAME') ?: 'root',
-    'password' => getenv('DB_PASSWORD') ?: '',
-    'database' => getenv('DB_DATABASE') ?: 'mydb',
+    'driver'   => $_ENV['DB_DRIVER'] ?? getenv('DB_DRIVER') ?: 'mysql',
+    'hostname' => $_ENV['DB_HOST'] ?? getenv('DB_HOST') ?: 'mysql-3cd37ed5-phoebeabante18.j.aivencloud.com',
+    'port'     => $_ENV['DB_PORT'] ?? getenv('DB_PORT') ?: '16717',
+    'username' => $_ENV['DB_USERNAME'] ?? getenv('DB_USERNAME') ?: 'avnadmin',
+    'password' => $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD') ?: 'AVNS_lwfP7XnONBwn5pCs5Fv',
+    'database' => $_ENV['DB_DATABASE'] ?? getenv('DB_DATABASE') ?: 'defaultdb',
     'charset'  => 'utf8mb4',
     'dbprefix' => '',
     'path'     => ''
